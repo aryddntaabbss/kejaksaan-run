2e11a7716416f7d3b57326aafc72914998291d44
aryaddinata <aryaddinataabbas017@gmail.com>
2024-12-20 00:41:34 +0900

    Update DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,7 +32,7 @@ class DatabaseSeeder extends Seeder
 
         Setting::create([
             'name' => 'footer',
-            'value' => '© Copyright Ternate Berlari 2024. All Rights Reserved.',
+            'value' => '© Copyright KEJAKSAAN FUN RUN 2024. All Rights Reserved.',
         ]);
         Setting::create([
             'name' => 'tanggal_event',
@@ -51,14 +51,14 @@ class DatabaseSeeder extends Seeder
             'biaya' => '350.000',
         ]);
 
-        
+
 
         DB::table('deskripsi_website')->insert([
             'url' => 'https://ternate-berlari.com/',
             'logo' => 'uploads/logo/qUIXXAuurXHnx0mb5k7uynDOXTcAsT91m2lvhaRm.png',
-            'title' => 'Ternate Berlari',
-            'keyword' => 'pesona ternate, ternate berlari',
-            'deskripsi' => 'Ternate Berlari adalah sebuah ajang lari yang menyatukan komunitas pecinta olahraga di Ternate, menawarkan pengalaman lari di antara pemandangan indah serta budaya kaya khas Kepulauan Maluku Utara. Melalui acara ini, kami berkomitmen menginspirasi gaya hidup sehat, mempererat kebersamaan, dan mempromosikan keindahan alam Ternate kepada masyarakat luas.',
+            'title' => 'KEJAKSAAN FUN RUN',
+            'keyword' => 'pesona ternate, KEJAKSAAN FUN RUN',
+            'deskripsi' => 'KEJAKSAAN FUN RUN adalah sebuah ajang lari yang menyatukan komunitas pecinta olahraga di Ternate, menawarkan pengalaman lari di antara pemandangan indah serta budaya kaya khas Kepulauan Maluku Utara. Melalui acara ini, kami berkomitmen menginspirasi gaya hidup sehat, mempererat kebersamaan, dan mempromosikan keindahan alam Ternate kepada masyarakat luas.',
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
@@ -78,7 +78,7 @@ class DatabaseSeeder extends Seeder
             'updated_at' => now(),
         ]);
         DB::table('tentang')->insert([
-            'deskripsi_tentang' => 'Ternate Berlari adalah sebuah ajang lari yang menyatukan komunitas pecinta olahraga di Ternate, menawarkan pengalaman lari di antara pemandangan indah serta budaya kaya khas Kepulauan Maluku Utara. Melalui acara ini, kami berkomitmen menginspirasi gaya hidup sehat, mempererat kebersamaan, dan mempromosikan keindahan alam Ternate kepada masyarakat luas.',
+            'deskripsi_tentang' => 'KEJAKSAAN FUN RUN adalah sebuah ajang lari yang menyatukan komunitas pecinta olahraga di Ternate, menawarkan pengalaman lari di antara pemandangan indah serta budaya kaya khas Kepulauan Maluku Utara. Melalui acara ini, kami berkomitmen menginspirasi gaya hidup sehat, mempererat kebersamaan, dan mempromosikan keindahan alam Ternate kepada masyarakat luas.',
             'gambar_tentang' => 'gambar_tentang/Cxa5okWzDdBghZ25F6hVOr6eOJqEViXT5qgtkwxn.png',
             'created_at' => now(),
             'updated_at' => now(),
@@ -130,15 +130,15 @@ class DatabaseSeeder extends Seeder
 
         Page::create([
             'name' => 'Syarat & Ketentuan',
-            'content' => '<div><strong>Peraturan dan Ketentuan Ternate Berlari 2024</strong><br>
-<br>#Semua Peserta diwajibkan Membaca dan Memahami Peraturan dan Ketentuan sebelum Mendaftar Untuk Event Ternate Berlari.<br>
-<br>1. Peserta Ternate Berlari adalah mereka yang mendaftar Resmi&nbsp; secara Online (Google Form) maupun secara Offline sebagai Peserta Ternate Berlari 2024.
+            'content' => '<div><strong>Peraturan dan Ketentuan KEJAKSAAN FUN RUN 2024</strong><br>
+<br>#Semua Peserta diwajibkan Membaca dan Memahami Peraturan dan Ketentuan sebelum Mendaftar Untuk Event KEJAKSAAN FUN RUN.<br>
+<br>1. Peserta KEJAKSAAN FUN RUN adalah mereka yang mendaftar Resmi&nbsp; secara Online (Google Form) maupun secara Offline sebagai Peserta KEJAKSAAN FUN RUN 2024.
 <br>
-<br>2. Peserta Ternate Berlari 2024 Kategori Atlet wajib mendaftarkan diri di kategori Elite Atlet, apabila ketahuan mendaftar di kategori Umum (Hobies) akan di diskualifikasi.
+<br>2. Peserta KEJAKSAAN FUN RUN 2024 Kategori Atlet wajib mendaftarkan diri di kategori Elite Atlet, apabila ketahuan mendaftar di kategori Umum (Hobies) akan di diskualifikasi.
 <br>
-<br>3. Event Ternate Berlari sifatnya Lifestyle, tidak terikat dengan peraturan Resmi PB PASI. Segala Peraturan dan Ketentuan ditetapkan oleh penyelenggara Event.
+<br>3. Event KEJAKSAAN FUN RUN sifatnya Lifestyle, tidak terikat dengan peraturan Resmi PB PASI. Segala Peraturan dan Ketentuan ditetapkan oleh penyelenggara Event.
 <br>
-<br>4. Event Ternate Berlari dibagi 3 Kategori, 5K Fun Run, 10K Race dan 21K Fun (Upgrade KM) bagi peserta yang ingin HM.
+<br>4. Event KEJAKSAAN FUN RUN dibagi 3 Kategori, 5K Fun Run, 10K Race dan 21K Fun (Upgrade KM) bagi peserta yang ingin HM.
 <br>
 <br>5. Kategori 10K Race, Penyelenggara akan menggunakan Juri/wasit yang berlisensi dari PASI.
 <br>
@@ -146,9 +146,9 @@ class DatabaseSeeder extends Seeder
 <br>
 <br>7. Hadiah Podium/Pemenang pada kategori masing-masing berupa uang tunai yang nominalnya telah ditentukan oleh penyelenggara.
 <br>
-<br>8. Peserta Ternate Berlari 2024 tidak memiliki riwayat penyakit yang membahayakan dan memastikan dirinya dalam kondisi sehat sebelum mendaftar dan saat pelaksanaan Event.
+<br>8. Peserta KEJAKSAAN FUN RUN 2024 tidak memiliki riwayat penyakit yang membahayakan dan memastikan dirinya dalam kondisi sehat sebelum mendaftar dan saat pelaksanaan Event.
 <br>
-<br>9. Penyelenggara Event Ternate Berlari dapat mengubah peraturan dan ketentuan sesuai dengan kebijakan sendiri tanpa pemberitahuan sebelumnya.
+<br>9. Penyelenggara Event KEJAKSAAN FUN RUN dapat mengubah peraturan dan ketentuan sesuai dengan kebijakan sendiri tanpa pemberitahuan sebelumnya.
 <br>
 <br>10. Hal-hal yang belum tercantum dalam peraturan dan ketentuan ini akan disampaikan kemudian oleh penyelenggara event.</div>',
             'slug' => 'syarat-ketentuan',
